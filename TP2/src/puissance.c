815966413d4190e94c6173b22e137d0998d4961e
hadourarezki80-ux <hadourarezki80@gmail.com>
2025-11-12 15:24:06 +0100

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;  
+    int b = 3;  
+    int resultat = 1;  
+
+    
+    for (int i = 1; i <= b; i++) {
+        resultat *= a;
+    }
+
+    printf("%d puissance %d = %d\n", a, b, resultat);
+
+    return 0;
+}
